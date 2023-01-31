b259c31dfe38f5030e50abe8ddb0ccb0c7a1f6a1
Matheus Angelicio <matheus.angelicio00@gmail.com>
2023-01-30 23:19:31 -0300

    validar campos

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+
+  final _formKey = GlobalKey<FormState>();
+
   final _tLogin = TextEditingController();
   final _tSenha = TextEditingController();
 
@@ -11,30 +14,39 @@ class LoginPage extends StatelessWidget {
   }
 
   _body() {
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: ListView(
-        children: [
-          _text("Login", "Digite o login", controller: _tLogin),
-          const SizedBox(height: 10,),
-          _text("Senha", "Digite a senha", password: true, controller: _tSenha),
-          const SizedBox(height: 20,),
-          _button("Login", () => _onClickLogin),
-        ],
+    return Form(
+      key: _formKey,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: ListView(
+          children: [
+            _text("Login", "Digite o login", controller: _tLogin, validator: _validateLogin),
+            const SizedBox(height: 10,),
+            _text(
+                "Senha", "Digite a senha", password: true, controller: _tSenha, validator: _validateSenha),
+            const SizedBox(height: 20,),
+            _button("Login", () => _onClickLogin),
+          ],
+        ),
       ),
     );
   }
 
-  _text(String label, String hint, {bool password = false, required TextEditingController controller}) {
+  _text(String label,
+      String hint,
+      {bool password = false,
+      required TextEditingController controller,
+      FormFieldValidator<String>? validator,}) {
     return TextFormField(
-      controller: controller,
-      obscureText: password,
-      style: const TextStyle(fontSize: 25, color: Colors.blue),
-      decoration: InputDecoration(
-          labelText: label,
-          labelStyle: const TextStyle(fontSize: 25, color: Colors.grey),
-          hintText: hint,
-          hintStyle: const TextStyle(fontSize: 16)),
+        controller: controller,
+        obscureText: password,
+        validator: validator,
+        style: const TextStyle(fontSize: 25, color: Colors.blue),
+    decoration: InputDecoration(
+    labelText: label,
+    labelStyle: const TextStyle(fontSize: 25, color: Colors.grey),
+    hintText: hint,
+    hintStyle: const TextStyle(fontSize: 16)),
     );
   }
 
@@ -51,9 +63,31 @@ class LoginPage extends StatelessWidget {
   }
 
   _onClickLogin() {
+    bool? formOk = _formKey.currentState?.validate();
+
+    if (formOk == null || formOk == false) {
+      return;
+    }
+
     String login = _tLogin.text;
     String senha = _tSenha.text;
 
     print("Login $login, Senha $senha");
+  }
+
+  String? _validateLogin(String? text) {
+    if (text == null || text.isEmpty){
+      return "Digite o Login";
+    }
+    return null;
+  }
+
+  String? _validateSenha(String? text) {
+    if (text == null || text.isEmpty){
+      return "Digite a senha";
+    } else if(text.length < 3) {
+      return "A senha precisa ter pelo menos 2 números";
+    }
+    return null;
   }
 }
