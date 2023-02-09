1be6eaafd8497d3381350874de0d889f6f692c69
Matheus Angelicio <matheus.angelicio00@gmail.com>
2023-02-09 16:05:04 -0300

    future builder

--- lib/pages/carro/carros_api.dart
+++ lib/pages/carro/carros_api.dart
@@ -2,8 +2,10 @@
 import 'carro.dart';
 
 class CarrosApi {
-  static List<Carro> getCarros() {
+  static Future<List<Carro>> getCarros() async {
     final carros = <Carro>[];
+
+    await Future.delayed(Duration(seconds: 2));
 
     carros.add(Carro(nome: "Ferrari FF", urlFoto: "https://s3-sa-east-1.amazonaws.com/videos.livetouchdev.com.br/esportivos/Maserati_Grancabrio_Sport.png"));
     carros.add(Carro(nome: "MERCEDES-BENZ C63 AMG", urlFoto: "https://s3-sa-east-1.amazonaws.com/videos.livetouchdev.com.br/esportivos/MERCEDES_BENZ_AMG.png"));
--- lib/pages/carro/home_page.dart
+++ lib/pages/carro/home_page.dart
@@ -18,18 +18,76 @@ class HomePage extends StatelessWidget {
   }
 
   _body() {
-    List<Carro> carros = CarrosApi.getCarros();
+    return FutureBuilder(
+      future: CarrosApi.getCarros(),
+      builder: (context, snapshot) {
 
-    return ListView.builder(
-        itemCount: carros.length,
-        itemBuilder: (context, index) {
-          Carro c = carros[index];
+        if(!snapshot.hasData){
+          return Center(child: CircularProgressIndicator(),);
+        } else {
+          List<Carro> carros = snapshot.data ?? List.empty();
+          return _listView(carros);
+        }
 
-          return Row(
-            children: [
-              Image.network(c.urlFoto ?? "", width: 150,)
-            ],
-          );
-        });
+      },
+    );
+  }
+
+  Container _listView(List<Carro> carros) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: ListView.builder(
+          itemCount: carros.length,
+          itemBuilder: (context, index) {
+            Carro c = carros[index];
+
+            return Card(
+              color: Colors.grey[100],
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: Image.network(
+                        c.urlFoto ?? "",
+                        width: 250,
+                      ),
+                    ),
+                    Text(
+                      c.nome ?? "",
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 25,
+                      ),
+                    ),
+                    const Text(
+                      "Descrição...",
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    ButtonTheme(
+                        child: ButtonBar(
+                      children: [
+                        TextButton(
+                          child: const Text("DETALHES"),
+                          onPressed: () {},
+                        ),
+                        TextButton(
+                          child: const Text("COMPARTILHAR"),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ))
+                  ],
+                ),
+              ),
+            );
+          }),
+    );
   }
 }
